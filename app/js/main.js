24b641750a2ccfd60ecfe8039c57c03201a02189
ann-phoenix <ann.phoenix.it@gmail.com>
2021-01-07 22:09:24 +0300

    added html, style and jquery for scroll to sections and scrollspy

---
+++ app/js/main.js
@@ -0,0 +1,61 @@
+$(function () {
+
+	let header = $('#header');
+	let headerH = header.innerHeight();
+	let scrollTop = $(window).scrollTop();
+	let headerMenuToggle = $('#headerMenuToggle');
+	let headerMenu = $('#header__menu');
+
+
+	/* Smooth scroll to sections */
+	$('[data-scroll]').on('click', function (event) {
+		event.preventDefault();
+
+		let scrollEl = $(this).data('scroll');
+		let scrollElPos = $(scrollEl).offset().top;
+
+		$('body').removeClass('show-header__menu');
+		headerMenuToggle.removeClass('active');
+		headerMenu.removeClass('show');
+
+		$('html, body').animate({
+			scrollTop: scrollElPos - headerH
+		}, 800);
+	});
+
+	/*Vertical navigation and scrollspy*/
+
+	//scrollSpy function
+	function scrollSpy() {
+		var sections = ['header', 'about', 'portfolio', 'services'];
+		var current;
+
+		for (var i = 0; i < sections.length; i++) {
+			if ($('#' + sections[i]).offset().top <= $(window).scrollTop()) {
+				current = sections[i];
+			}
+		}
+
+		$("nav a[href='#" + current + "']").addClass('active');
+		$("nav a").not("a[href='#" + current + "']").removeClass('active');
+	}
+
+	// smooth scrolling navigation
+	$("nav a").click(function () {
+		var target = $(this).attr("href");
+		$("body, html").animate({
+			scrollTop: $(target).offset().top
+		}, 700);
+		return false;
+	});
+
+	//scrollSpy call
+	$(document).ready(function () {
+		scrollSpy();
+	});
+
+	$(window).scroll(function () {
+		scrollSpy();
+	});
+
+});
